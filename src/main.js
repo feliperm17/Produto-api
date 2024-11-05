7f0ec1660df43825b68563786dab4f346afc5b5c
feliperm17 <felipe.r.moran@gmail.com>
2024-11-05 15:54:47 -0300

    cors

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
+import cors from "cors";
+
 import { route } from "./routes/index.js"
 
 dotenv.config ({path: ".env"})
@@ -7,6 +9,8 @@ dotenv.config ({path: ".env"})
 const app = express();
 app.use(express.json())
 app.use(route)
+app.use(cors());
+
 
 app.use((err, req, res, next) => {
     res.status(500).json({
