a3e9c5245c631517fefcf02779e0c809a169f5cb
feliperm17 <felipe.r.moran@gmail.com>
2024-11-06 19:46:36 -0300

    a

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
-const Pool = require('pg').Pool;
+require('dotenv').config(); // Importa o dotenv e carrega as variáveis de ambiente
+const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "produtos",
-    password: "123456",
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
 });
 
 module.exports = pool;
--- src/main.js
+++ src/main.js
@@ -1,26 +1,32 @@
-import express from "express"
-import dotenv from "dotenv"
+import express from "express";
+import dotenv from "dotenv";
 import cors from "cors";
 
-import { route } from "./routes/index.js"
+import { route } from "./routes/index.js";
 
-dotenv.config ({path: ".env"})
+dotenv.config({ path: ".env" });
 
 const app = express();
-app.use(express.json())
-app.use(route)
+
+// Configuração do CORS antes das rotas
 app.use(cors());
 
+// Middleware para interpretar JSON
+app.use(express.json());
 
+// Configuração das rotas
+app.use(route);
+
+// Middleware de tratamento de erros
 app.use((err, req, res, next) => {
-    res.status(500).json({
-        message: `Internal Server Error - ${err.message}`
-    });
-    next();
-})
+  res.status(500).json({
+    message: `Internal Server Error - ${err.message}`
+  });
+});
 
+// Definindo a porta e inicializando o servidor
 const PORT = process.env.APP_PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Servidor não esta conectado na Porta ${PORT}`)
-})
+  console.log(`Servidor conectado na Porta ${PORT}`);
+});
